f8058e381de18e9bd1dafba390f3e3f975541166
yasser <yasser@192.168.1.5>
2018-09-27 22:49:05 +0100

    ads management app

---
+++ adsManagement/src/main/java/com/management/ads/AdsManagementApplication.java
@@ -0,0 +1,12 @@
+package com.management.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsManagementApplication.class, args);
+	}
+}
